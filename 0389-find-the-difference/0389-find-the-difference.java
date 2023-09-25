90e73233fc720f99120e5e6f1bbb6086134446c5
Yash Salunke <91377974+YAS-18@users.noreply.github.com>
2023-09-25 20:52:30 +0530

    Time: 2 ms (68.98%), Space: 40.1 MB (99.77%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,21 +1,15 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character,Integer> map1 = new HashMap<>();
-        Map<Character,Integer> map2 = new HashMap<>();
-        int i=0;
-        for(; i<s.length() ; i++) {
-            map1.put(s.charAt(i) , map1.getOrDefault(s.charAt(i),0)+1);
-            map2.put(t.charAt(i) , map2.getOrDefault(t.charAt(i),0)+1);
+        long sSum = 0;
+        long tSum = 0;
+
+        for(int i=0 ; i<s.length() ; i++){
+            sSum += s.charAt(i);
+            tSum += t.charAt(i);
         }
 
-        map2.put(t.charAt(i) , map2.getOrDefault(t.charAt(i),0)+1);
-        
-        char ans = 'a';
-        for(i=0 ; i<26 ; i++){
-            ans = (char)(97 + i);
-            if(map1.get(ans) != map2.get(ans))
-                break;
-        }
-        return ans;
+        tSum += t.charAt(t.length()-1);
+
+        return (char)(tSum - sSum);
     }
 }
